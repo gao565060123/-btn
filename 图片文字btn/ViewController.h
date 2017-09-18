ff70cd6e3248f52a1671c5de02bbabc279c60405
gao565060123 <565060123@qq.com>
2017-09-18 09:46:20 +0800

    Initial Commit

---
+++ 图片文字btn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片文字btn
+//
+//  Created by mybook on 2017/9/18.
+//  Copyright © 2017年 石塔Mr.高超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 图片文字btnTests/____btnTests.m
@@ -0,0 +1,39 @@
+//
+//  ____btnTests.m
+//  图片文字btnTests
+//
+//  Created by mybook on 2017/9/18.
+//  Copyright © 2017年 石塔Mr.高超. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ____btnTests : XCTestCase
+
+@end
+
+@implementation ____btnTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
